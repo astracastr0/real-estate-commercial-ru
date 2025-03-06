0c5edf4e9a2166e4681d42793129ba1666160342
Anastasiia S <71646787+astracastr0@users.noreply.github.com>
2025-03-07 00:42:32 +0200

    Update __init__.py

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,1 +1,3 @@
-
+from .script1_get_sale_rent_offers import main as fetch_offers
+from .script2_rent_readjs import process_rent_jsons as process_rent
+from .script2_sale_readjs import process_sale_jsons as process_sale
