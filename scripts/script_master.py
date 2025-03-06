5c5d6602128de49b02baba8a01e6ee5501a5c0f7
Anastasiia S <71646787+astracastr0@users.noreply.github.com>
2025-03-07 00:51:19 +0200

    Update script_master.py

--- scripts/script_master.py
+++ scripts/script_master.py
@@ -9,10 +9,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "s
 from script1_get_sale_rent_offers import main as fetch_offers
 from script2_rent_readjs import process_rent_jsons as process_rent
 from script2_sale_readjs import process_sale_jsons as process_sale
-# from scripts import fetch_offers, process_rent, process_sale
-# from scripts.script1_get_sale_rent_offers import main as fetch_offers
-# from scripts.script2_rent_readjs import process_rent_jsons as process_rent
-# from scripts.script2_sale_readjs import process_sale_jsons as process_sale
 from scripts.script3_cian_join_sale_rent import join_sale_rent
 from scripts.script4_google_nearby_cat import enrich_dataset  # Ensure this import works correctly
 
