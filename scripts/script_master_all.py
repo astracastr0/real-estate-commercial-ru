cd57e7a184017727374ef02e34a908b58932e2bd
Anastasiia S <71646787+astracastr0@users.noreply.github.com>
2025-03-10 07:54:15 +0200

    Update script_master_all.py

--- scripts/script_master_all.py
+++ scripts/script_master_all.py
@@ -32,8 +32,8 @@ def run_script_master_for_areas(areas, api_key, script_master_path, output_direc
     print(f"Combined CSV file saved to {combined_csv_path}")
 
 if __name__ == "__main__":
-    areas = [   'NAO', 'CAO','VAO']
-    #  'ZAO', 'SAO','SZAO', 'SVAO', 'UVAO', 'UAO',  'UZAO','ZelAO'
+    areas = [   'NAO', 'CAO','VAO', 'ZAO', 'SAO','SZAO', 'SVAO', 'UVAO', 'UAO',  'UZAO','ZelAO']
+    #  
     api_key = 'AIzaSyD3uB5Syh7E-tW0a9qLu2EHJ1MqHxqyUu8'  # Replace with your actual API key
     script_master_path = 'scripts/script_master.py'  # Adjust the path to your script_master.py
     output_directory = 'output/csv'  # Directory where the combined CSV should be saved
