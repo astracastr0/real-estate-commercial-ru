55f7dec97ccc942ba11812c3be23347123718cbd
Anastasiia S <71646787+astracastr0@users.noreply.github.com>
2025-03-23 13:22:37 +0200

    Update script_master_all.py

--- scripts/script_master_all.py
+++ scripts/script_master_all.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     areas = ['NAO', 'CAO', 'VAO', 'ZAO', 'SAO', 'SZAO', 'SVAO', 'UVAO', 'UAO', 'UZAO', 'ZelAO']
 
     api_key = 'AIzaSyD3uB5Syh7E-tW0a9qLu2EHJ1MqHxqyUu8'  # Replace with your actual API key
-    script_master_path = 'scripts/script_master.py'  # Adjust the path to your script_master.py
+    script_master_path = 'real-estate-scripts/scripts/script_master.py'  # Adjust the path to your script_master.py
     output_directory = 'output/csv'  # Directory where the combined CSV should be saved
 
     run_script_master_for_areas(areas, api_key, script_master_path, output_directory)
