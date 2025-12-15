63014fd3724303543d72966fd1c980f4de300830
Anastasiia S <71646787+astracastr0@users.noreply.github.com>
2025-12-15 03:23:27 +0200

    Disable running areas and sending in script

--- scripts/script_master_all.py
+++ scripts/script_master_all.py
@@ -286,10 +286,10 @@ def send_email_with_file(file_path, to_email):
 if __name__ == "__main__":
     # CONTROL SWITCHES
     run_parts = {
-        "run_areas": True,
+        "run_areas": False,
         "combine": True,
         "delta": True,
-        "send": True,
+        "send": False,
         "bitrix": True
         }
 
